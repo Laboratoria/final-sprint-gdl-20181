dfa0c70d3556501d935a00410e00ef99a5a6636f
memecast <amecgavante1305@gmail.com>
2018-07-31 12:37:05 -0500

    Fase uno: Setup de una aplicacion de nuestro espacio de trabajo para REACT.js

--- react-track/solucionarios/tic-tac-toe/src/index.js
+++ react-track/solucionarios/tic-tac-toe/src/index.js
@@ -1,8 +1,14 @@
+/* SETUP de tu entorno local 
+https://reactjs.org/tutorial/tutorial.html#setup-option-2-local-development-environment 
+    1. Despues de crear tu app de react a través de create-react-app 
+    borra los documentos dentro de la carpeta src.
+    2. Crea un archivo index.js (requerido) y un archivo main.css (requerido)
+    3. En el archivo index.js (Este mismo) escribe el siguiente "encabezado".
+*/
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+/* Al hacer esto estamos trayendo a nuestro espacio de desarrollo las funcionalidades y
+atributos de REACT y REACTDOM (La dependencia que permite manejar el DOM virtual de REACT)
+*/
