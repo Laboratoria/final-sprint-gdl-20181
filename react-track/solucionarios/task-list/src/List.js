f8c063329535873e2799435e22632ebe01b9f043
Levhita <levhita@gmail.com>
2018-08-19 18:19:06 -0500

    updated mocks and commented unused code that was triggering an eslint alert

--- react-track/solucionarios/task-list/src/List.js
+++ react-track/solucionarios/task-list/src/List.js
@@ -1,10 +1,10 @@
 import React from "react";
-import firebase from "firebase";
+//import firebase from "firebase";
 
 class List extends React.Component {
-    constructor(props){
+    /*constructor(props){
         super(props);
-    }
+    }*/
     
     render (){
         return <h1>Logged in as {this.props.user.email}</h1>
